ed7ab872cbe5ef39ecb99de676c4bd857cc4bc37
davidscdev <davidsc.dev@gmail.com>
2021-09-21 08:18:55 -0300

    Inserido os comentários no código JS.

--- script/script.js
+++ script/script.js
@@ -1,10 +1,13 @@
+// api_key=c0c5fe318f67b8420d23fc5a0baa3718   --> Código de acesso oa TDBM
+
+//Constantes de acesso ao tmdb.
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c0c5fe318f67b8420d23fc5a0baa3718&language=pt-BR';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=c0c5fe318f67b8420d23fc5a0baa3718&query="';
 const LINGUAGE_API = '&language=pt-BR';
 
 
-
+// Seleção de elementos para construção/manipulação do HTML.
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 const main = document.getElementById('main');
@@ -12,19 +15,22 @@ const main = document.getElementById('main');
 //Pega a lista inicial de filmes
 getMovies(API_URL);
 
+//Função para buscar os filmes na API
 async function getMovies(url) {
-    const res = await fetch(url);
-    const data = await res.json();
+    const res = await fetch(url); //Percorre o conteúdo disponibilizado pela API 
+    const data = await res.json(); // gera um Json com os dados.
 
+    //Chama a função pra exibir o conteúdo em tela.
     showMovies(data.results);
-
 }
 
+
+//Função para exibir os filmes em tela.
 function showMovies(movies) {
     main.innerHTML = '';
 
     movies.forEach(movie => {
-        const { title, poster_path, vote_average, overview } = movie;
+        const { title, poster_path, vote_average, overview } = movie; //Descontruindo o array de filmes.
 
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
@@ -46,6 +52,8 @@ function showMovies(movies) {
     });
 }
 
+
+//Estiliza a nota de acordo com a média
 function getClassByRate(vote) {
     if (vote >= 8) {
         return 'green';
@@ -56,6 +64,8 @@ function getClassByRate(vote) {
     }
 }
 
+
+//Funcionalidade do item de pesquisa 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
